f8926b1b82e6ad419073612ca825160c1701876c
김용원 <dydvn@naver.com>
2022-05-20 00:09:07 +0900

    test

--- Project_RC/Assets/_Scripts/TestScript.cs
+++ Project_RC/Assets/_Scripts/TestScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,6 +13,6 @@ public class TestScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 아아
     }
 }
